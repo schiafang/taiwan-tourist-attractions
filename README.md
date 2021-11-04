9a296196fcf566b873b47cdff8fc4c211cd8176f
schiafang <schiafang@gmail.com>
2021-11-04 17:56:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guided Tour of Tourist Attractions in Taiwan
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <p>Guided Tour of Taiwan Attractions</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
