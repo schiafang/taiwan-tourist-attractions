365d625512e23c70050de9b0c29b101606def802
schiafang <schiafang@gmail.com>
2021-11-14 17:00:50 +0800

    feat: redux slice

--- src/slices/tourism.js
+++ src/slices/tourism.js
@@ -4,31 +4,69 @@ const initialState = {
   Activity: [],
   Restaurant: [],
   ScenicSpot: [],
+  search: {
+    type: '',
+    city: '',
+    data: [],
+  },
+  categories: {
+    Activity: [],
+    Restaurant: [],
+    ScenicSpot: [],
+  },
 }
 
 export const createInitialData = createAction('CREATE_INITIAL_DATA')
 
-export const getActivities = createAction('GET_ACTIVITIES')
+export const setActivities = createAction('SET_ACTIVITIES')
 
-export const getRestaurant = createAction('GET_RESTAURANTS')
+export const setRestaurant = createAction('SET_RESTAURANTS')
 
-export const getScenicSpots = createAction('GET_SCENIC_SPOTS')
+export const setScenicSpots = createAction('SET_SCENIC_SPOTS')
+
+export const setFilteredData = createAction('SET_FILTERED_DATA')
 
 const tourismSlice = createSlice({
   name: 'tourism',
   initialState,
   extraReducers: {
     ['CREATE_INITIAL_DATA']: (state, action) => {
-      return action.payload
+      let currentCategories = JSON.parse(JSON.stringify(state.categories))
+      const concatCategories = (type, filed) => {
+        let currentValue = currentCategories[type]
+        let newValue = action.payload[type].map((i) =>
+          i[filed] ? i[filed] : null
+        )
+        let concat = [...currentValue, ...newValue].filter(
+          (item, index, arr) => item && arr.indexOf(item) === index
+        )
+        return concat
+      }
+
+      return {
+        ...state,
+        ...action.payload,
+        categories: {
+          Activity: concatCategories('Activity', 'Class1'),
+          Restaurant: concatCategories('Restaurant', 'Class'),
+          ScenicSpot: concatCategories('ScenicSpot', 'Class1'),
+        },
+      }
     },
-    ['GET_ACTIVITIES']: (state, action) => {
+    ['SET_ACTIVITIES']: (state, action) => {
       return { ...state, Activity: action.payload }
     },
-    ['GET_RESTAURANTS']: (state, action) => {
+    ['SET_RESTAURANTS']: (state, action) => {
       return { ...state, Restaurant: action.payload }
     },
-    ['GET_SCENIC_SPOTS']: (state, action) => {
+    ['SET_SCENIC_SPOTS']: (state, action) => {
       return { ...state, ScenicSpot: action.payload }
+    },
+    ['SET_FILTERED_DATA']: (state, action) => {
+      return { ...state, search: action.payload }
+    },
+    ['SET_CATEGORIES_DATA']: (state, action) => {
+      return { ...state, search: action.payload }
     },
   },
 })
