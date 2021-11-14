8a1d2792d98e988dabfb1354d8d6d9db854791fe
schiafang <schiafang@gmail.com>
2021-11-14 16:57:52 +0800

    add: assets

---
+++ src/assets/category-bg/index.js
@@ -0,0 +1,47 @@
+import scenicSpot01 from './bg1-1.png'
+import scenicSpot02 from './bg1-2.png'
+import scenicSpot03 from './bg1-3.png'
+import scenicSpot04 from './bg1-4.png'
+import scenicSpot05 from './bg1-5.png'
+import scenicSpot06 from './bg1-6.png'
+import scenicSpot07 from './bg1-7.png'
+import activity01 from './bg2-1.png'
+import activity02 from './bg2-2.png'
+import activity03 from './bg2-3.png'
+import activity04 from './bg2-4.png'
+import activity05 from './bg2-5.png'
+import activity06 from './bg2-6.png'
+import restaurant01 from './bg3-1.png'
+import restaurant02 from './bg3-2.png'
+import restaurant03 from './bg3-3.png'
+import restaurant04 from './bg3-4.png'
+import restaurant05 from './bg3-5.png'
+import restaurant06 from './bg3-6.png'
+
+export default {
+  ScenicSpot: [
+    scenicSpot01,
+    scenicSpot02,
+    scenicSpot03,
+    scenicSpot04,
+    scenicSpot05,
+    scenicSpot06,
+    scenicSpot07,
+  ],
+  Activity: [
+    activity01,
+    activity02,
+    activity03,
+    activity04,
+    activity05,
+    activity06,
+  ],
+  Restaurant: [
+    restaurant01,
+    restaurant02,
+    restaurant03,
+    restaurant04,
+    restaurant05,
+    restaurant06,
+  ],
+}
---
+++ src/styles/config.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export default styled
+
+export const breakpoints = {
+  mobile: '(max-width: 768px)',
+  desktop: '(min-width: 768px)',
+}
